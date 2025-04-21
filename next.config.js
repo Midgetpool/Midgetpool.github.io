7e5400cf38941ba63fe8621172ac65fdfeb171f7
Midget_pool <137927488+Midgetpool@users.noreply.github.com>
2025-04-21 16:20:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	compiler: {
-		styledComponents: true,
-	},
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-
-	webpack: (config, options) => {
-		config.module.rules.push({
-			test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
-			type: "asset/resource",
-			generator: {
-				filename: "static/chunks/[path][name].[hash][ext]",
-			},
-		});
-
-		return config;
-	},
-
-	staticPageGenerationTimeout: 6000
+  output: 'export', // 👈 important
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/chunks/[path][name].[hash][ext]",
+      },
+    });
+    return config;
+  },
+  staticPageGenerationTimeout: 6000
 };
 
 module.exports = nextConfig;
